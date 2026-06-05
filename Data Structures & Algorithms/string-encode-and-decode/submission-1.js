dfd4c75c67d51fd670dfa59b1db5f042d4dc3139
abhishekaydav <108981872+abhishekaydav@users.noreply.github.com>
2026-06-05 22:19:43 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = ""
+
+        for(let i=0; i<strs.length; i++){
+            encoded = encoded + strs[i].length+"#" +strs[i]
+        }
+
+        return encoded
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(s) {
+        const result=[]
+
+        while(s.length > 0){
+            let firstHash = s.indexOf("#")
+            if(firstHash === -1) break;
+            const strLength = s.substring(0, firstHash)
+            const length = Number(strLength)
+
+            const startIndex = firstHash +1 
+
+            const lastIndex = startIndex + length
+
+            const word = s.substring(startIndex,lastIndex)
+
+            result.push(word)
+
+            s = s.substring(lastIndex)
+
+        }
+
+        return result
+    }
+}
